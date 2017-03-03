070652ffda67bf2625310c585f41e919b060d1da
Guangtao Zhao <v-gzhao@microsoft.com>
2017-03-03 18:05:53 +0800

    Initial commit.

---
+++ SingletonMode/SingletonMode/Emperor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonMode
+{
+    public class Emperor
+    {
+        private static readonly Emperor _instance = new Emperor();
+
+        public static object _lock = new object();
+
+        private Emperor() { }
+
+        public static Emperor Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public void Say()
+        {
+            Console.WriteLine("I'm QianLong.");
+        }
+    }
+}
