0b649154621967725e933ffce36841bead237aeb
Guangtao Zhao <v-gzhao@microsoft.com>
2017-03-07 11:14:51 +0800

    Another singleton mode

---
+++ SingletonMode/SingletonMode/AzureStorageManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonMode
+{
+    public class AzureStorageManager
+    {
+        public static AzureStorageManager _instance = null;
+
+        public static object _lock = new object();
+
+        private AzureStorageManager() { }
+
+        public static AzureStorageManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+
+                        if (_instance == null)
+                        {
+                            try
+                            {
+                                _instance = new AzureStorageManager();
+                            }
+                            catch (Exception e)
+                            {
+                                throw e;
+                            }
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
